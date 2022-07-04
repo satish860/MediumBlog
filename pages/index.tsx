fa5b61e7f6b5db9802eff79211949f413e9a5f82
satish <satish860@gmail.com>
2022-07-04 13:26:41 +0530

    Adding the Metatags for SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,42 @@ const Home: NextPage<PostProps> = ({ posts }: PostProps) => {
       <Head>
         <title>Medium</title>
         <link rel="icon" href="/favicon.ico" />
+        <title>Medium Clone</title>
+        <meta name="title" content="Medium Clone" />
+        <meta
+          name="description"
+          content="A Clone of Medium using NextJs and Sanity from Satish Venkatakrishnan"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://medium-blog-iota.vercel.app/"
+        />
+        <meta property="og:title" content="Medium Clone" />
+        <meta
+          property="og:description"
+          content="A Clone of Medium using NextJs and Sanity from Satish Venkatakrishnan"
+        />
+        <meta
+          property="og:image"
+          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://medium-blog-iota.vercel.app/"
+        />
+        <meta property="twitter:title" content="Medium Clone" />
+        <meta
+          property="twitter:description"
+          content="A Clone of Medium using NextJs and Sanity from Satish Venkatakrishnan"
+        />
+        <meta
+          property="twitter:image"
+          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+        />
       </Head>
 
       <Header />
